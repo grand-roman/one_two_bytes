97829e5195f460f0cb0b2121f8794b5d3ae2e655
grand-roman@yandex.ru <grand-roman@yandex.ru>
2020-09-18 18:10:40 +0700

    Update one_two_bytes.py

--- one_two_bytes.py
+++ one_two_bytes.py
@@ -3,24 +3,24 @@
  @return 1(bytes) | 2(bytes)
 '''
 def find_last_bit(mas):
-    if mas[-1]==0:
+    if mas[-1] == 0:
         return 2
     len_mas = len(mas)
     temp = []
     for i in reversed(range(len_mas)):
-        if mas[i]!=0:
+        if mas[i] != 0:
             temp.append(mas[i])
         else:
-            if mas[i-1]==1:
+            if mas[i-1] == 1:
                 temp.append(mas[i])
                 break
             else:
                 temp.append(mas[i])
     res = 0
-    kol_bit=0
+    kol_bit = 0
     for i in reversed(temp):
-        if i==1:
-            if kol_bit==1:
+        if i == 1:
+            if kol_bit == 1:
                 res = 2
             else:
                 res = 1
@@ -29,7 +29,7 @@ def find_last_bit(mas):
             if kol_bit > 1:
                 kol_bit = 1
             else:
-                kol_bit +=1
+                kol_bit += 1
     return res
 
 print(find_last_bit([0,1,1,1,0,0,0,0,0,0,0,0,0,0,0]))
